889f4b440c53ac2681862751b3516d281d77d180
Denis Oliveira <39929010+sinedolvra@users.noreply.github.com>
2021-10-03 20:58:53 -0300

    remove code smell

--- src/Todo.Api/Program.cs
+++ src/Todo.Api/Program.cs
@@ -5,14 +5,14 @@ using Microsoft.Extensions.Hosting;
 namespace Todo.Api
 {
     [ExcludeFromCodeCoverage]
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
