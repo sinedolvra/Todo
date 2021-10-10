ee33c4367e8b89c28641ecf20edc53f7d1ac8e6a
Denis Oliveira <39929010+sinedolvra@users.noreply.github.com>
2021-10-10 09:52:22 -0300

    Create TodoQueries.cs

---
+++ src/Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<TodoItem, bool>> GetUnDone()
+        {
+            return todo => todo.Done == false;
+        }
+        
+        public static Expression<Func<TodoItem, bool>> GetDone()
+        {
+            return todo => todo.Done;
+        }
+        
+        public static Expression<Func<TodoItem, bool>> GetAll()
+        {
+            return todo => true;
+        }
+    }
+}
