d89710ea58466e339e29d4bdefd0d8a84b5eb2a7
Denis Oliveira <39929010+sinedolvra@users.noreply.github.com>
2021-10-10 09:52:28 -0300

    Add new routes

--- src/Todo.Api/TodoController.cs
+++ src/Todo.Api/TodoController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
 using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Repositories;
 using Todo.Shared;
 
 namespace Todo.Api
@@ -14,10 +15,12 @@ namespace Todo.Api
     public class TodoController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ITodoRepository _repository;
 
-        public TodoController(IMediator mediator)
+        public TodoController(IMediator mediator, ITodoRepository repository)
         {
             _mediator = mediator;
+            _repository = repository;
         }
         
         [Route("")]
@@ -29,12 +32,46 @@ namespace Todo.Api
                 var result = (GenericCommandResult) await _mediator.Send(request);
                 if (!result.Success) return BadRequest(result);
 
-                return Ok(result);
+                return CreatedAtRoute("", result);
             }
             catch (Exception e)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponse(e.Message));
             }
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetTodo(string id)
+        {
+            var todo = await _repository.Get(id);
+            if (todo is null) return NotFound(new ErrorResponse($"No todo was found with this id {id}"));
+            
+            return Ok(todo);
+        }
+        
+        [Route("")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var todos = await _repository.GetAll();
+            return Ok(todos);
+        }
+        
+        [Route("undone")]
+        [HttpGet]
+        public async Task<IActionResult> GetUnDone()
+        {
+            var todos = await _repository.GetUnDone();
+            return Ok(todos);
+        }
+        
+        [Route("done")]
+        [HttpGet]
+        public async Task<IActionResult> GetDone()
+        {
+            var todos = await _repository.GetDone();
+            return Ok(todos);
         }
     }
 }
